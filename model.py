41987402b3d0348c39aa79e0befb716043d9da73
fvmassoli <fabiovaleriomassoli@gmail.com>
2017-04-20 16:34:14 +0200

    added new files

--- model.py
+++ model.py
@@ -1,136 +1,80 @@
-import csv
-import cv2
-import numpy as np
 from keras.models import Sequential
-from keras.layers import Lambda, Flatten, Dense, Dropout, Activation
-from keras.layers.convolutional import Convolution2D, Cropping2D, Conv2D
-from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Flatten, Dense, Lambda, Dropout
+from keras.layers.convolutional import Convolution2D, Cropping2D
 from keras.regularizers import l2
-from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.callbacks import EarlyStopping
 from keras.optimizers import Adam
-#from keras.initializers import TruncatedNormal, Zeros
+import pydot
+from utils import read_file, prepare_dataset
+from keras.utils import plot_model
 import matplotlib.pyplot as plt
-from utils import read_file, preprare_dataset, training_data_generator, validation_data_generator
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
 
-base_path = 'simulator_outputs/track_1/'
-lines = read_file(base_path)
-features, labels, input_shape = preprare_dataset(base_path, lines)
-print('Input shape: ' + str(input_shape))
+lines = read_file()
+features, labels = prepare_dataset(lines)
 
-NB_EPOCHS = 1
-BATCH_SIZE = 128
-VALIDATION_BATCH_SIZE = 64
-DROPOUT_PROB = 0.5
-REGULARIZATION_FACTOR = 0.02
-LEARNING_RATE = 0.001
-VALIDATION_SPLIT = 0.3
-
-nb_kernels_1 = 3
-nb_kernels_2 = 24
-nb_kernels_3 = 36
-nb_kernels_4 = 48
-nb_kernels_5 = 64
-
-kernel_size_1 = 5
-kernel_size_2 = 3
-
-stride_1=(2, 2)
-stride_2=(1, 1)
-
-weights_initializer = None#TruncatedNormal(mean=0.0, stddev=1.e-2, seed=None)
-
-biases_initializer = None#Zeros
-
-activation_function = Activation('elu')
+NB_EPOCHS=15
+BATC_SIZE=128
+LEARNING_RATE=0.0001
+VALIDATION_SPLIT=0.2
+REGULARIZATION_FACTOR=0.001
+DROPOUT_PROB=0.3
 
 fc1_output_size = 1164
 fc2_output_size = 100
 fc3_output_size = 50
 fc4_output_size = 10
+fc5_output_size = 1
 
 model = Sequential()
-model.add(Cropping2D(cropping=((70,25),(0,0)), input_shape=(160, 320, 3)))
-model.add(Convolution2D(nb_kernels_1, 5, 5, activation='elu'))
-# model.add(Convolution2D(nb_kernels_2, 5, 5, activation='elu'))
-# model.add(Convolution2D(nb_kernels_3, 5, 5, activation='elu'))
-# model.add(Convolution2D(nb_kernels_4, 3, 3, activation='elu'))
-# model.add(Convolution2D(nb_kernels_5, 3, 3, activation='elu'))
+
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70,24),(60,60))))
+model.add(Convolution2D(24, (5, 5), strides=(2,2), activation='relu', kernel_regularizer=l2(REGULARIZATION_FACTOR)))
+model.add(Convolution2D(36, (5, 5), strides=(2,2), activation='relu', kernel_regularizer=l2(REGULARIZATION_FACTOR)))
+model.add(Convolution2D(48, (5, 5), strides=(2,2), activation='relu', kernel_regularizer=l2(REGULARIZATION_FACTOR)))
+model.add(Convolution2D(64, (3, 3), activation='relu', kernel_regularizer=l2(REGULARIZATION_FACTOR)))
+model.add(Convolution2D(64, (3, 3), activation='relu', kernel_regularizer=l2(REGULARIZATION_FACTOR)))
 model.add((Flatten()))
-#model.add(Dense(fc1_output_size))
-#model.add(Dropout(DROPOUT_PROB))
-# model.add(Dense(fc2_output_size))
-# model.add(Dropout(DROPOUT_PROB))
-# model.add(Dense(fc3_output_size))
-# model.add(Dropout(DROPOUT_PROB))
-# model.add(Dense(fc4_output_size))
-# model.add(Dropout(DROPOUT_PROB))
-# model.add(Dense(fc5_output_size))
-# model.add(Flatten())
-# model.add(Dense(1))
+model.add(Dense(1164, activation='relu', kernel_regularizer=l2(REGULARIZATION_FACTOR)))
+model.add(Dropout(DROPOUT_PROB))
+model.add(Dense(100, activation='relu', kernel_regularizer=l2(REGULARIZATION_FACTOR)))
+model.add(Dropout(DROPOUT_PROB))
+model.add(Dense(50, activation='relu', kernel_regularizer=l2(REGULARIZATION_FACTOR)))
+model.add(Dropout(DROPOUT_PROB))
+model.add(Dense(10, activation='elu', kernel_regularizer=l2(REGULARIZATION_FACTOR)))
+model.add(Dropout(DROPOUT_PROB))
 model.add(Dense(1))
 
 model.compile(loss='mse',
-               optimizer='adam',
-               metrics=['accuracy'])
+              optimizer='adam',#Adam(lr=LEARNING_RATE),
+              metrics=['accuracy'])
 
-history = model.fit(features,
-                    labels,
-                    0.2,
-                    shuffle=True,
-                    nb_epoch=NB_EPOCHS)
+early_stopping_callback = EarlyStopping(monitor='val_loss',
+                                        min_delta=1.e-4,
+                                        patience=1,
+                                        mode='min')
+history_object = model.fit(features,
+                           labels,
+                           epochs=NB_EPOCHS,
+                           batch_size=BATC_SIZE,
+                           validation_split=VALIDATION_SPLIT,
+                           shuffle=True,
+                           verbose=1,
+                           callbacks=[early_stopping_callback])
 
-
-#
-# print('Start training...')
-#
-# training_generator = training_data_generator(features, labels)
-# validation_enerator = validation_data_generator(features, labels)
-
-#checkpoint = ModelCheckpoint('model.h5', monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=False, mode='auto')
-#early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=1.e-4, verbose=1, mode='auto')
-#
-# model.compile(loss='mse',
-#                optimizer=Adam(lr=LEARNING_RATE),
-#                metrics=['accuracy'])
-
-# history = model.fit_generator(training_generator,
-#                                steps_per_epoch=batch_size,
-#                                epochs=nb_epochs,
-#                                validation_data=validation_enerator,
-#                                validation_steps=validation_batch_size,
-#                                callbacks=[checkpoint, early_stopping])
-#
-# history = model.fit(features,
-#                     labels,
-#                     validation_split=VALIDATION_SPLIT,
-#                     shuffle=True,
-#                     nb_epoch=NB_EPOCHS,
-#                     verbose=1)
-#                    callbacks=EarlyStopping(monitor='val_loss',
-#                                            min_delta=1.e-4,
-#                                            patience=2,
-#                                            verbose=1,
-#                                            mode='auto'))
-#
-# model.save('model.h5')
-#
-# print('Neural Net ready!!!')
+model.summary()
+model.save('model2.h5')
+SVG(model_to_dot(model).create(prog='dot', format='svg'))
+plot_model(model, to_file='model.png', show_shapes=True, show_layer_names=True)
 
 
-
-
-
-
-
-#print(history.history)
-
-# list all data in history
-#print(history.history.keys())
-
-#plt.plot(history.history['loss'])
-#plt.plot(history.history['val_loss'])
-#plt.title('model mean squared error loss')
-#plt.ylabel('mean squared error loss')
-#plt.xlabel('epoch')
-#plt.legend(['training set', 'validation set'], loc='upper right')
-#plt.show()
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.savefig('model_loss_hisotry.png')
